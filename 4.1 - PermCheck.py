ce877195839f8a7830fdfaff5c6179c2e236f6aa
ThalysonR <ThalysonR.castro@hotmail.com>
2019-10-03 16:55:34 -0400

    Update 4.1 - PermCheck.py

--- 4.1 - PermCheck.py
+++ 4.1 - PermCheck.py
@@ -1,12 +1,6 @@
 def solution(A):
-    nDict = {}
     sA = sorted(A)
-    for n in sA:
-        nCount = nDict.get(n,0)
-        if nCount > 0:
+    for i in range(1, max(sA[-1:][0], len(A)) + 1):
+        if i != sA[i - 1]:
             return 0
-        nDict[n] = nCount + 1
-    if len(A) == sA[-1:][0]:
-        return 1
-    else:
-        return 0
+    return 1
