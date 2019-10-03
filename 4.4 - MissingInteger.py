d70d3dcedd862e2fea3b0a5f2b221b85776558ad
Thalyson <ThalysonR.castro@hotmail.com>
2019-10-03 08:26:29 -0400

    missing integer 100%

--- 4.4 - MissingInteger.py
+++ 4.4 - MissingInteger.py
@@ -1,10 +1,7 @@
 def solution(A):
     sA = sorted(A)
-    maxA = sA[-1:][0]
-    if maxA < 1:
-        return 1
-    minN = maxA
-    for i in range(len(A) - 1):
-        if minN > sA[i] + 1 and sA[i + 1] - sA[i] > 1:
-            minN = sA[i] + 1
-    return minN if minN < maxA else maxA + 1
+    smallest = 1
+    for n in sA:
+        if n == smallest:
+            smallest += 1
+    return smallest
